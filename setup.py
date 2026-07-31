8972d7ffaa0c70ab27d07696dc3d116425312462
Richard <richytong@gmail.com>
2026-07-31 08:42:33 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+# from __init__ import __version__
+
+setup(
+    name='functionalthreading',
+    version='0.1.0',
+    description='Functional programming with thread-based parallelism.',
+    # packages=['__init__'],
+)
