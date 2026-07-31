a2194434af4e3a5a5f8d2c0c8e49e73b5c6b8b1f
Richard <richytong@gmail.com>
2026-07-31 13:04:28 -0700

    tmap

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,5 +1,5 @@
-from typing import overload
-import threading
+from threading import Thread
+import itertools
 
 def _chain(argument, funcs):
     ret = argument
@@ -7,41 +7,35 @@ def _chain(argument, funcs):
         ret = func(ret)
     return ret
 
-@overload
-def chain(*funcs):
-    '''
-Returns a function of chained functions that expects the arguments to the first function. Each function in the chain is evaluated in series starting from the first function, passing the return value as the first and only argument to the next function. The return value of the chain is the return value of the last function.
-
-```python
-my_function_chain = chain(
-    lambda m, n: m + n + 1,
-    lambda n: n ** 2,
-    lambda n: n / 3,
-    print
-)
-
-my_function_chain(2, 3)
-```
-    '''
-    inner_funcs = (argument,) + funcs
-    def inner_func(inner_argument):
-        return _chain(inner_argument, inner_funcs)
-    return inner_func
-
 def chain(argument, *funcs):
-    '''
-Chains functions together. Each function is evaluated in series starting from the first function, passing the return value as the first and only argument to the next function. The return value of the chain is the return value of the last function.
+    """Chain functions together.
 
-```python
-chain(
-    2,
-    lambda n: n + 1,
-    lambda n: n ** 2,
-    lambda n: n / 3,
-    print
-)
-```
-    '''
+    Each function is evaluated in series starting from the first function, passing the return value as the first and only argument to the next function. The return value of the chain is the return value of the last function.
+
+    ```python
+    chain(
+        2,
+        lambda n: n + 1,
+        lambda n: n ** 2,
+        lambda n: n / 3,
+        print
+    )
+    ```
+
+    If the first non-function argument is omitted, returns a function of chained functions that expects the non-function argument.
+
+    ```python
+    my_function_chain = chain(
+        lambda n: n + 1,
+        lambda n: n ** 2,
+        lambda n: n / 3,
+        print
+    )
+
+    my_function_chain(2)
+    ```
+
+    """
     if callable(argument):
         inner_funcs = (argument,) + funcs
         def inner_func(inner_argument):
@@ -50,24 +44,64 @@ chain(
     return _chain(argument, funcs)
 
 def tap(func):
-    '''
-Calls a function with an argument, returning the argument.
+    """Call a function with an argument, returning the argument.
 
-```python
-chain(
-    1,
-    lambda n: n + 1,
-    tap(print),
-    lambda n: n + 2,
-    tap(print),
-    lambda n: n + 3,
-    print
-)
-```
-    '''
+    ```python
+    chain(
+        1,
+        lambda n: n + 1,
+        tap(print),
+        lambda n: n + 2,
+        tap(print),
+        lambda n: n + 3,
+        print
+    )
+    ```
+
+    """
     def inner_func(argument):
         func(argument)
         return argument
     return inner_func
 
-__all__ = ['chain', 'tap']
+class _Thread(Thread):
+    def __init__(self, *args, **kwargs):
+        Thread.__init__(self, *args, **kwargs)
+        self.value = None
+        self.target = kwargs['target']
+        self.args = kwargs['args']
+
+    def run(self):
+        self.value = self.target(*self.args)
+
+def tmap(array_or_tuple, func):
+    """Map a function concurrently across each element of an array or tuple.
+
+    Each function invokation happens in a separate thread.
+
+    ```python
+    squared = tmap([1, 2, 3], lambda n: n ** 2)
+    ```
+
+    """
+    length = len(array_or_tuple)
+    threads = []
+    index = 0
+    args = [array_or_tuple]
+    while index < length:
+        t = _Thread(target=func, args=[array_or_tuple[index]])
+        t.start()
+        threads.append(t)
+        index += 1
+    index = 0
+    result = []
+    while index < length:
+        t = threads[index]
+        t.join()
+        result.append(t.value)
+        index += 1
+    if isinstance(array_or_tuple, tuple):
+        return tuple(result)
+    return result
+
+__all__ = ['chain', 'tap', 'tmap']
--- functions/test.py
+++ functions/test.py
@@ -1,7 +1,8 @@
-from __init__ import chain, tap
+from __init__ import chain, tap, tmap
+import time
 
 def test_chain():
-    assert chain.__doc__, 'Should have docs'
+    assert chain.__doc__, 'Should have docs.'
 
     ret = chain(
         1,
@@ -20,7 +21,7 @@ def test_chain():
     assert ret == 12, '12'
 
 def test_tap():
-    assert tap.__doc__, 'Should have docs'
+    assert tap.__doc__, 'Should have docs.'
 
     ret = tap(lambda n: n + 1)(1)
     assert ret == 1, f'{ret} != 1'
@@ -34,9 +35,32 @@ def test_tap():
     assert ret == 1, f'{ret} != 1'
     assert n == 1, f'{n} != 1'
 
+def test_tmap():
+    assert tmap.__doc__, 'Should have docs.'
+
+    ret = tmap([1, 2, 3], lambda n: n ** 2)
+    assert ret[0] == 1, f'ret[0] ({ret[0]}) != 1'
+    assert ret[1] == 4, f'ret[1] ({ret[1]}) != 4'
+    assert ret[2] == 9, f'ret[2] ({ret[2]}) != 9'
+
+    def f(n):
+        time.sleep(1)
+        return n + 1
+
+    start = time.time()
+    ret = tmap([1, 2, 3], f)
+    end = time.time()
+
+    assert ret[0] == 2, f'ret[0] ({ret[0]}) != 2'
+    assert ret[1] == 3, f'ret[1] ({ret[1]}) != 3'
+    assert ret[2] == 4, f'ret[2] ({ret[2]}) != 4'
+    assert end - start < 1.1, 'Took too much time.'
+
+
 def test():
     test_chain()
     test_tap()
+    test_tmap()
 
 if __name__ == '__main__':
     test()
